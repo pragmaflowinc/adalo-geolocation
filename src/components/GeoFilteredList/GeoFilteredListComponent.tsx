7b4d88cd786cbe53444551410324a22902e55619
Steven McKeown <steven@pragmaflow.com>
2021-09-08 15:25:13 -0400

    fixed distance to not be in meters

--- src/components/GeoFilteredList/GeoFilteredListComponent.tsx
+++ src/components/GeoFilteredList/GeoFilteredListComponent.tsx
@@ -17,12 +17,12 @@ export function GeoFilteredListComponent(props: GeoFilteredListProps) {
   if (listProps.items && listProps.items.length > 0 && listProps.items[0].secondLine?.displayDistance) {
     listProps.items = listProps.items.reduce((acc, item) => {
       if (item.secondLine) {
-        const distance = getDistance(
+        const distance = Math.floor(getDistance(
           { latitude: props.currentLatitude!, longitude: props.currentLongitude! },
           { latitude: item.geoData?.itemLatitude!, longitude: item.geoData?.itemLongitude! }
-        )
-        item.secondLine.text = `${distance / (props.unitsOfMeasure === 'imperial' ? METER_TO_MILE : METER_TO_KILOMETER)}${props.unitsOfMeasure === 'imperial' ? 'mi' : 'km'}${item.secondLine && item.secondLine.text ? ` - ${item.secondLine.text}` : ''}`
-        if (props.currentRadius && distance < props.currentRadius) {
+        ) / (props.unitsOfMeasure === 'imperial' ? METER_TO_MILE : METER_TO_KILOMETER))
+        item.secondLine.text = `${distance}${props.unitsOfMeasure === 'imperial' ? 'mi' : 'km'}${item.secondLine && item.secondLine.text ? ` - ${item.secondLine.text}` : ''}`
+        if (props.currentRadius && distance <= props.currentRadius) {
           acc.push(item)
         }
       }
