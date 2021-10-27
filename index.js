f03eaf8bf920c9332015c439cf2d6ebbe8bd0792
Steven McKeown <steven@pragmaflow.com>
2021-10-27 10:26:21 -0400

    renamed component

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 // Named export must match component name in package.json
-export { default as GeoLocation } from './build/components/GeoLocation'
-export { default as GeoFilteredList } from './build/components/GeoFilteredList'
+export { default as GeoLocationPF } from './build/components/GeoLocation'
+export { default as GeoFilteredListPF } from './build/components/GeoFilteredList'
