7fc625b2b04d320e220d9d9ae52372abc3250862
Steven McKeown <steven@pragmaflow.com>
2021-11-08 13:51:12 -0500

    Fixed the ios install script

--- scripts/install_ios.sh
+++ scripts/install_ios.sh
@@ -5,7 +5,12 @@ set -x
 yarn add react-native-geolocation-service
 yarn add @voximplant/react-native-foreground-service
 
+name=$PROJECT_NAME
 
-plutil -insert NSLocationWhenInUseUsageDescription -string 'Location permission when in use' info.plist
+if grep -q "<key>NSLocationWhenInUseUsageDescription" ios/$name/Info.plist; then
+  echo "Location already supported, nothing to do here."
+else
+  plutil -insert NSLocationWhenInUseUsageDescription -string 'Location permission when in use' ios/$name/Info.plist
+fi
 
 echo "configured iOS settings"
