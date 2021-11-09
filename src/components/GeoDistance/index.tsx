d7844a30aac86edfed10655f26dfcb610c4f27e2
Steven McKeown <steven@pragmaflow.com>
2021-11-09 16:24:56 -0500

    New componet
    Fixed my mistake on pushing with the 2 on componets
    added checks to make sure the component is configured before calculating distance (toString error)
    Added GeoDistance

---
+++ src/components/GeoDistance/index.tsx
@@ -0,0 +1,48 @@
+import { getDistance } from "geolib";
+import React, { useEffect, useState } from "react";
+import { View, StyleSheet, Text } from "react-native";
+import { RowProps, SimpleList } from "../SimpleList";
+import { GeoDistanceProps } from "./generated";
+
+const METER_TO_MILE = 1609.34;
+const METER_TO_KILOMETER = 1000;
+
+const GeoDistance = (props: GeoDistanceProps) => {
+  const [distance, setDistance] = useState(0)
+  useEffect(() => {
+    const { firstLatitude = 0, firstLongitude = 0, secondLatitude = 0, secondLongitude = 0, unitsOfMeasure = 'imperial' } = props
+      const calDistance = Math.floor(
+        getDistance(
+          {
+            latitude: firstLatitude,
+            longitude: firstLongitude,
+          },
+          {
+            latitude: secondLatitude,
+            longitude: secondLongitude,
+          }
+        ) /
+          (unitsOfMeasure === "imperial"
+            ? METER_TO_MILE
+            : METER_TO_KILOMETER)
+      );
+      setDistance(calDistance)
+
+      if (props.onCalculated) {
+        props.onCalculated(calDistance)
+      }
+  }, [props])
+  return (
+    <View style={styles.wrapper}>
+      <Text>{`${distance}`}</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+	wrapper: {
+		display: 'flex',
+	}
+})
+
+export default GeoDistance
